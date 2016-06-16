9ff0141c1a0fd710329350688a62fc6257e82f1b
Chushu <liuchushu10@gmail.com>
2016-06-16 15:03:22 +0800

    finished ex6

--- machine-learning-ex6/ex6/emailFeatures.m
+++ machine-learning-ex6/ex6/emailFeatures.m
@@ -57,5 +57,13 @@ x = zeros(n, 1);
 
 % =========================================================================
     
+for i = 1:n
+    for j = 1:length(word_indices)
+        if (i == word_indices(j))
+            x(i) = 1;
+        end
+    end
+end
+
 
 end
