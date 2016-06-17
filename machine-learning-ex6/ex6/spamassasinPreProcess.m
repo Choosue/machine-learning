cd2de603c26ceea856351191a79c67e77ea95296
Leo Choosue <liuchushu10@gmail.com>
2016-06-17 19:09:08 +0800

    spamassasin pre-process

--- machine-learning-ex6/ex6/spamassasinPreProcess.m
+++ machine-learning-ex6/ex6/spamassasinPreProcess.m
@@ -3,21 +3,17 @@ file_path = "./spamassasin-dataset/easy_ham/";
 save_name = "easyham.mat";
 
 % Number of Examples
-m = 2;
+m = 2500;
 
-% Read Files
+% Read File Names
 file_names = readFileName(m, strcat(file_path, "cmds"));
-
-% Store all the examples into X
-X = zeros(m, 1899);
-y = zeros(m, 1);
 
 dots = 12;
 
 for i = 1:m
     file_contents = readFile(strcat(file_path, file_names{i}));
-    word_indices  = processEmailSilent(file_contents);
-    X(i, :)       = emailFeatures(word_indices);
+    word_array    = processEmailSilent(file_contents);
+    save(strcat(strcat(file_path, "processed_email/"), file_names{i}), "word_array");
     
     fprintf('.');
     dots = dots + 1;
@@ -30,5 +26,3 @@ for i = 1:m
     end
 end
 fprintf(' Done! \n\n');
-
-save(save_name, "X", "y");
